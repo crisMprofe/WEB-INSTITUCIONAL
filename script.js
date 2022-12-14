ef268c603c648435cc57da6ed23aec4b30139e7a
Taty71 <cristinbmaia@gmail.com>
2022-12-13 22:45:36 -0300

    Cambio en h2 Ficha...

---
+++ script.js
@@ -0,0 +1,8 @@
+const boton=document.getElementById('boton')
+
+boton.addEventListener('click', () => {
+    console.log('CLICK')
+    swal("Sistema de registro y control de asistencia en proceso")
+    /*alert("Sistema de registro y control de asistencia en proceso")*/
+    
+}) 
