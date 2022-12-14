34800c09140e04357161c9a06d4e2ece48d87a04
Taty71 <cristinbmaia@gmail.com>
2022-12-13 23:48:31 -0300

    Agregó IG, script inscripción

---
+++ includes/script.js
@@ -0,0 +1,11 @@
+
+alert("Sistema de registro en proceso, por favor no realizar carga de datos")
+
+/*const boton=document.getElementById('boton')
+
+boton.addEventListener('click', () => {
+    console.log('CLICK')
+    /*swal("Sistema de registro y control de asistencia en proceso")
+    alert("Sistema de registro y control de asistencia en proceso")
+    
+}) */
--- script.js
+++
@@ -1,8 +0,0 @@
-const boton=document.getElementById('boton')
-
-boton.addEventListener('click', () => {
-    console.log('CLICK')
-    swal("Sistema de registro y control de asistencia en proceso")
-    /*alert("Sistema de registro y control de asistencia en proceso")*/
-    
-}) 
